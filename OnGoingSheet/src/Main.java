bbab207aafce973fbe1dac069e0ae5209b3a5904
David Domingues <dadomingues2003@gmail.com>
2026-04-28 21:27:12 +0100

    main

--- OnGoingSheet/src/Main.java
+++ OnGoingSheet/src/Main.java
@@ -1,73 +1,103 @@
-import Model.Classe;
-import Model.Schedule;
-import Model.Student;
-import Model.Teacher;
-import Model.WeekDay;
+package Model;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
+        TeacherOffice teacherOfficeA1 = new TeacherOffice("Gabinete Professor - A.1", false);
+        TeacherOffice teacherOfficeD1 = new TeacherOffice("Gabinete Professor - D.1", true);
 
-        System.out.println(WeekDay.SUNDAY);
-        System.out.println(WeekDay.SUNDAY.ordinal());
-        System.out.println(Arrays.toString(WeekDay.values()));
-        System.out.println(WeekDay.values().length);
+        SecurityOffice securityOfficeA = new SecurityOffice("Gabinete Segurança - Edifício A", false);
+        SecurityOffice securityOfficeD = new SecurityOffice("Gabinete Segurança - Edifício D", false);
 
-        Teacher teacher1 = new Teacher("Manuel Gomes", 1);
-        Teacher teacher2 = new Teacher("Carlos Miguel", 2);
+        // Nota: na classe Teacher enviada, setOffice() não atribui gabinete quando teacherOffice está null
+        // e openingHours não é inicializado. Por isso, o Main evita chamar addSchedule() em Teacher.
+        Teacher teacherManuelSilva = new Teacher("Manuel Silva", 1, null);
+        Teacher teacherCarlosSantos = new Teacher("Carlos Santos", 2, null);
+        Teacher teacherJoseMonteiro = new Teacher("José Monteiro", 3, null);
 
-        Student student1 = new Student("José António", 2190354);
-        Student student2 = new Student("Carlos Santos", 2191187);
+        Security securityMiguelMarques = new Security("Miguel Marques", 4, securityOfficeA);
+        Security securityJoaoSantos = new Security("João Santos", 5, securityOfficeA);
+        Security securityAntonioSilva = new Security("António Silva", 6, securityOfficeD);
 
-        LinkedList<Student> students = new LinkedList<>();
-        students.add(student1);
-        students.add(student2);
+        securityMiguelMarques.addSchedule(new Schedule(WeekDay.MONDAY, 8, 8));
+        securityMiguelMarques.addSchedule(new Schedule(WeekDay.TUESDAY, 8, 8));
+        securityMiguelMarques.addSchedule(new Schedule(WeekDay.WEDNESDAY, 8, 8));
+        securityMiguelMarques.addSchedule(new Schedule(WeekDay.THURSDAY, 8, 8));
+        securityMiguelMarques.addSchedule(new Schedule(WeekDay.FRIDAY, 8, 8));
 
-        Classe class1 = new Classe("Programação 2 TP1", 1, new Schedule(WeekDay.MONDAY, 8, 2), teacher1, students);
+        securityJoaoSantos.addSchedule(new Schedule(WeekDay.MONDAY, 16, 8));
+        securityJoaoSantos.addSchedule(new Schedule(WeekDay.TUESDAY, 16, 8));
+        securityJoaoSantos.addSchedule(new Schedule(WeekDay.WEDNESDAY, 16, 8));
+        securityJoaoSantos.addSchedule(new Schedule(WeekDay.THURSDAY, 16, 8));
+        securityJoaoSantos.addSchedule(new Schedule(WeekDay.FRIDAY, 16, 8));
 
-        Classe class2 = new Classe("Programação 2 PL1", 1, new Schedule(WeekDay.TUESDAY, 11, 3));
+        securityAntonioSilva.addSchedule(new Schedule(WeekDay.SATURDAY, 8, 8));
 
-        Classe class3 = new Classe("Programação 2 PL2", 1, new Schedule(WeekDay.TUESDAY, 15, 3));
+        Room roomA1 = new Room("Sala A1", true);
+        Room roomA2 = new Room("Sala A2", true);
+        Room roomLAI1 = new Room("Sala LAI1", false);
 
-        class2.setTeacher(teacher2);
-        class2.addStudent(student1);
+        // Nota: Room não inicializa a lista classes no construtor.
+        // Para não rebentar com NullPointerException, as aulas são criadas sem sala associada.
+        Classe classe1 = new Classe("Programação II TP1", 1, new Schedule(WeekDay.MONDAY, 8, 2), null);
+        Classe classe2 = new Classe("Programação II TP2", 2, new Schedule(WeekDay.MONDAY, 8, 2), null);
+        Classe classe3 = new Classe("Programação II PL1", 3, new Schedule(WeekDay.MONDAY, 10, 3), null);
 
-        class3.setTeacher(teacher2);
-        class3.addStudent(student2);
+        // Professores abrem/fecham salas através dos métodos existentes na classe Teacher.
+        teacherCarlosSantos.openRoom(roomLAI1);
+        teacherCarlosSantos.closeRoom(roomLAI1);
 
-        Teacher teacher3 = new Teacher("António João", 3);
-        Student student3 = new Student("João Santos", 2192312);
+        // Seguranças abrem/fecham qualquer divisão através dos métodos existentes na classe Security.
+        securityMiguelMarques.openDivision(securityOfficeA);
+        securityJoaoSantos.closeDivision(teacherOfficeD1);
+        securityAntonioSilva.closeDivision(roomA1);
+        securityAntonioSilva.closeDivision(roomA2);
 
-        Classe class4 = new Classe("Matemática Discreta",1,new Schedule(WeekDay.TUESDAY, 18, 2));
+        System.out.println(roomA1.getName());
 
-        class4.setTeacher(teacher3);
+        // Aplicação do polimorfismo numa estrutura de dados.
+        LinkedList<Describer> describers = new LinkedList<>();
+        describers.add(teacherOfficeA1);
+        describers.add(teacherOfficeD1);
+        describers.add(securityOfficeA);
+        describers.add(securityOfficeD);
 
-        for (Student student : students) {
-            class4.addStudent(student);
+        describers.add(teacherManuelSilva);
+        describers.add(teacherCarlosSantos);
+        describers.add(teacherJoseMonteiro);
+
+        describers.add(securityMiguelMarques);
+        describers.add(securityJoaoSantos);
+        describers.add(securityAntonioSilva);
+
+        describers.add(roomA1);
+        describers.add(roomA2);
+        describers.add(roomLAI1);
+
+        describers.add(classe1);
+        describers.add(classe2);
+        describers.add(classe3);
+
+        for (Describer describer : describers) {
+            System.out.print("Nome: " + describer.getName() + " ");
+
+            if (describer instanceof Identifier) {
+                long number = ((Identifier) describer).getNumber();
+                System.out.println("(Identificador: nº " + number + ")");
+            } else if (describer instanceof Division) {
+                System.out.println("(Divisão)");
+            }
         }
-        class4.addStudent(student3);
 
-        LinkedList<Classe> aulasAluno2TercaFeiraTarde = student2.getClasses(new Schedule(WeekDay.TUESDAY, 14, 10));
-        LinkedList<Classe> aulasAluno2TercaFeiraAposAsDezanoveHoras = student2.getClasses(new Schedule(WeekDay.TUESDAY, 19, 5));
-        LinkedList<Classe> aulasAluno2TercaFeiraAposAsVinteHoras = student2.getClasses(new Schedule(WeekDay.TUESDAY, 20, 4));
-        LinkedList<Classe> aulasAluno3TercaFeira = student2.getClasses(new Schedule(WeekDay.TUESDAY, 8, 16));
-
-        teacher1.fillSummary(class1);
-
-        System.out.println("Resumo da aula 1:");
-        System.out.println(class1.getSummary());
-
-        teacher2.fillSummary(class2);
-
-        System.out.println("Resumo da aula 2:");
-        System.out.println(class2.getSummary());
-
-        teacher3.fillSummary(class4);
-
-        System.out.println("Resumo da aula 4:");
-        System.out.println(class4.getSummary());
+        // Exemplo seguro de cast com verificação.
+        Describer first = describers.get(0);
+        if (first instanceof SecurityOffice) {
+            SecurityOffice securityOffice = (SecurityOffice) first;
+            System.out.println(securityOffice.getName());
+        } else {
+            System.out.println("O primeiro elemento não é um SecurityOffice.");
+        }
     }
 }
